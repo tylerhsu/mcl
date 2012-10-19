abc35a8e46dffcc07aef8079f30b6e20874b9ea2
Tyler Hsu <tyler.l.hsu@gmail.com>
2012-10-19 22:02:21 +0000

    localizer is implemented except for the sensory update step

--- public/js/main.js
+++ public/js/main.js
@@ -4,39 +4,83 @@
     var keysPressed = {};
 
     /**
-     * Robot
+     * Mcl
+     * 
+     * Monte Carlo localizer
      */
-    var Robot = function(world) {
-        this.world = world;
-        this.state = State.generate(world);
-        this.radius = 25;
-        this.scanDistance = 1000;
-        this.driveSpeed = 2;
-        this.rotateSpeed = 0.1;
+    var Mcl = function(agent) {
+        this.agent = agent;
+        this.belief = [];
+        this.numberOfSamples = 100;
+
+        this.initialize();
     };
 
-    Robot.prototype = {
-        draw: function(gfx) {
-            gfx.drawCircle(this.state.x, this.state.y, this.radius, { fillStyle: '#8ED6FF' });
-            this.state.draw(gfx);
+    Mcl.prototype = {
+        initialize: function() {
+            var n, x, y, angle;
+            for (n = 0; n < this.numberOfSamples; n++) {
+                x = Math.random() * this.agent.world.width;
+                y = Math.random() * this.agent.world.height;
+                angle = Math.random() * 2 * Math.PI;
+                this.belief.push(new State(x, y, angle));
+            }
         }
+
+        , tick: function() {
+            var n
+            ,   totalWeight
+            ,   weights = new Array(this.belief.length)
+            ,   newBelief = new Array(this.belief.length);
+
+            for (n = 0; n < this.belief.length; n++) {
+                this.belief[n] = this.agent.predict(this.belief[n]);
+                weights[n] = this.agent.updateBelief(this.belief[n]);
+            }
+
+            // resample
+            for (n = 0; n < this.belief.length; n++) {
+                newBelief[n] = util.sample(this.belief, weights);
+            }
+
+            this.belief = newBelief;
+        }
+
+        , draw: function(gfx) {
+            var n, state;
+            for (n = 0; n < this.belief.length; n++) {
+                state = this.belief[n];
+                state.dotRadius = 1;
+                state.rayLength = 0;
+                state.draw(gfx);
+            }
+        }
+    };
+
+    var Rangefinder = function() {
+        this.maxDistance = 1000;
+        this.resolution = 20;
+        this.noise = 5;
+    };
+    
+    Rangefinder.prototype = {
 
         /*
          * Returns the euclidean distance to the nearest obstacle in the
          * given direction from the robot, up to maxDistance.  A return value
          * of maxDistance implies that no obstacle was encountered.
          */
-        , castRay: function(imageData, angle, maxDistance) {
-            var x0 = this.state.x
-            ,   y0 = this.state.y
-            ,   x1 = this.state.x + Math.cos(angle) * maxDistance
-            ,   y1 = this.state.y + Math.sin(angle) * maxDistance
-            ,   distance = maxDistance;
+        castRay: function(map, state, angle) {
+            var x0 = state.x
+            ,   y0 = state.y
+            ,   x1 = state.x + Math.cos(angle) * this.maxDistance
+            ,   y1 = state.y + Math.sin(angle) * this.maxDistance
+            ,   distance = this.maxDistance;
             util.traceLine(x0, y0, x1, y1, function(x, y) {
-                var pixelOffset = (x + y * imageData.width) * 4
-                ,   red = imageData.data[pixelOffset]
-                ,   green = imageData.data[pixelOffset + 1]
-                ,   blue = imageData.data[pixelOffset + 2];
+                var pixelOffset = (x + y * map.width) * 4
+                ,   red = map.data[pixelOffset]
+                ,   green = map.data[pixelOffset + 1]
+                ,   blue = map.data[pixelOffset + 2];
 
                 // wall pixel?
                 if (red > 240 && green < 5 && blue < 5) {
@@ -51,29 +95,94 @@
         }
 
         /*
-         * Casts _resolution_ rays, radially equidistant, from the robot's center.
+         * Casts _resolution_ rays, radially equidistant, from the given state.
          * Returns an array containing the distances reported by each of the rays
          * to the nearest obstacle in that direction, starting from the ray cast forward,
-         * relative to the robot's heading, and moving clockwise.
+         * relative to the state's heading, and moving clockwise.
          */
-        , scan: function(resolution) {
-            var increment = (2 * Math.PI) / resolution
-            ,   angle = this.state.angle
-            ,   imageData = this.world.context.getImageData(0, 0, this.world.context.canvas.width, this.world.context.canvas.height)
-            ,   distances = [];
+        , scan: function(map, state, noise) {
+            var increment = (2 * Math.PI) / this.resolution
+            ,   noise = noise || this.noise
+            ,   angle = state.angle
+            ,   distances = []
+            ,   dist;
 
-            for (var n = 0; n < resolution; n++) {
-                distances.push(this.castRay(imageData, angle, this.scanDistance));
+            for (var n = 0; n < this.resolution; n++) {
+                dist = this.castRay(map, state, angle);
+                dist *= Math.random() * noise + (1 - noise / 2);
+                distances.push(dist);
                 angle += increment;
             }
 
             return distances;
+        }
+    };
+
+    /**
+     * Robot
+     */
+    var Robot = function(world) {
+        this.world = world;
+        this.map = this.world.context.getImageData(0, 0, this.world.context.canvas.width, this.world.context.canvas.height);
+        this.sensor = new Rangefinder();
+        this.state = State.generate(world);
+        this.odometer = { dx: 0, dy: 0, dangle: 0 };
+        this.radius = 25;
+        this.driveSpeed = 2;
+        this.rotateSpeed = 0.1;
+        this.odometerNoise = 5;
+    };
+
+    Robot.prototype = {
+        draw: function(gfx) {
+            gfx.drawCircle(this.state.x, this.state.y, this.radius, { fillStyle: '#8ED6FF' });
+            this.state.draw(gfx);
+        }
+
+        /*
+         * Given a previous state, predicts & returns a new state based on odometry since the
+         * last tick.
+         */
+        , predict: function(state) {
+            var newState = new State(
+                state.x + this.odometer.dx,
+                state.y + this.odometer.dy,
+                state.angle + this.odometer.dangle
+            );
+            return newState;
+        }
+
+        , resetOdometer: function() {
+            this.odometer.dx = 0;
+            this.odometer.dy = 0;
+            this.odometer.dangle = 0;
+        }
+
+        /*
+         * Record odometry data, introducing optional noise.
+         */
+        , recordOdometry: function(dx, dy, dangle) {
+            // apply noise
+            dx = dx * (Math.random() * this.odometerNoise + (1 - this.odometerNoise / 2));
+            dy = dy * (Math.random() * this.odometerNoise + (1 - this.odometerNoise / 2));
+            dangle = dangle * (Math.random() * this.odometerNoise + (1 - this.odometerNoise / 2));
+
+            //record readings
+            this.odometer.dx += dx;
+            this.odometer.dy += dy;
+            this.odometer.dangle += dangle;
+        }
+
+        , scan: function() {
+            this.sensor.scan(this.map, this.state);
         }
 
         /*
          * Simulate one physics timestep
          */
         , simulate: function() {
+            var newX, newY, newAngle;
+
             this.ddrive = 0;
             this.drotate = 0;
 
@@ -94,10 +203,30 @@
                 this.drotate = this.rotateSpeed;
             }
 
+            // calculate new position
+            newX = this.state.x + Math.cos(this.state.angle) * this.ddrive;
+            newY = this.state.y + Math.sin(this.state.angle) * this.ddrive;
+            newAngle = this.state.angle + this.drotate;
+
+            this.recordOdometry(
+                newX - this.state.x,
+                newY - this.state.y,
+                newAngle - this.state.angle
+            );
+
             // update position
-            this.state.x = this.state.x + Math.cos(this.state.angle) * this.ddrive;
-            this.state.y = this.state.y + Math.sin(this.state.angle) * this.ddrive;
-            this.state.angle = this.state.angle + this.drotate;
+            this.state.x = newX;
+            this.state.y = newY;
+            this.state.angle = newAngle;
+        }
+
+        /*
+         * Given a predicted state, returns a weighting coefficient
+         * that expresses the likelihood of the state given current sensory information.
+         */
+        , weightBelief: function(state) {
+            // 'true' scan, done by ray tracing on a known map
+            var mapScan = this.sensor.scan(this.map, state, 0);
         }
     };
 
@@ -128,15 +257,17 @@
     };
 
     State.prototype = {
-        draw: function(gfx) {
+        draw: function(gfx, options) {
+            var options = options || {};
             // draw the dot
-            gfx.drawCircle(this.x, this.y, this.dotRadius);
+            gfx.drawCircle(this.x, this.y, this.dotRadius, options);
             // draw the ray
             gfx.drawLine(
                 this.x,
                 this.y,
                 this.x + Math.cos(this.angle) * this.rayLength,
-                this.y + Math.sin(this.angle) * this.rayLength
+                this.y + Math.sin(this.angle) * this.rayLength,
+                options
             );
         }
     };
@@ -169,7 +300,8 @@
         ,   context = canvas.getContext('2d')
         ,   gfx = new Graphics(context)
         ,   world = new World(canvas.width, canvas.height, worldMap, context)
-        ,   robot = new Robot(world);
+        ,   robot = new Robot(world)
+        ,   localizer = new Mcl(robot);
 
         // keyboard listeners
         document.onkeydown = function(e) {
@@ -180,19 +312,34 @@
             keysPressed[e.keyCode.toString()] = false;
         };
 
+        document.onkeypress = function(e) {
+            // space bar
+            if (e.keyCode === 32) {
+                tick();
+            }
+        };
+
         setInterval(function() {
             simulate();
             draw();
         }, 25);
 
+        // simulate one universal timestep
         function simulate() {
             robot.simulate();
+        }
+
+        // execute one algorithmic timestep
+        function tick() {
+            localizer.tick();
+            robot.resetOdometer();
         }
 
         function draw() {
             gfx.clear();
             world.draw(gfx);
             robot.draw(gfx);
+            localizer.draw(gfx);
         }
     };
 }(worldMap, Graphics, util));
--- public/js/util.js
+++ public/js/util.js
@@ -1,6 +1,86 @@
 var util = (function() {
     var util = {
         /*
+         * Returns a copy of the given array with its values
+         * normalized so they sum to 1.
+         */
+        normalize: function(arr) {
+            var n
+            ,   val
+            ,   factor
+            ,   sum = this.sum(arr)
+            ,   newArr = [];
+            for (n = 0; n < arr.length; n++) {
+                val = (arr[n] / sum);
+                newArr.push(val);
+            }
+            return newArr;
+        }
+
+        /*
+         * Returns a random sample from the given set, where the probability of
+         * returning set[n] is weighted by weights[n].
+         */
+        , sample: function(set, weights) {
+            var n
+            ,   min = 1000
+            ,   delta = 1
+            ,   rand
+            ,   sample
+            ,   sum = 0
+            ,   cdf = [];
+
+            if (set.length != weights.length) {
+                console.error('Warning: number of elements in set (' + set.length + ') differs from number of weights supplied (' + weights.length + ')');
+            }
+
+            weights = this.normalize(weights);
+
+            for (n = 0; n < weights.length; n++) {
+                cdf[n] = sum;
+                sum += weights[n];
+            }
+
+            rand = Math.random();
+            
+            n = 0;
+            while (cdf[n] < rand && n < cdf.length) {
+                delta = rand - cdf[n];
+                if (delta < min) {
+                    sample = set[n];
+                    min = delta;
+                }
+                n++;
+            }
+            
+            return sample;
+        }
+
+        , testSample: function(set, weights, precision) {
+            var n
+            ,   counts = {};
+            set.forEach(function(el) {
+                counts[el.toString()] = 0;
+            });
+            for (n = 0; n < 100000; n++) {
+                counts[util.sample(set, weights, precision).toString()]++;
+            }
+            return counts;
+        }
+
+        /*
+         * Returns the sum of the values in the given array
+         */
+        , sum: function(arr) {
+            var n
+            ,   sum = 0;
+            for (n = 0; n < arr.length; n++) {
+                sum += arr[n];
+            }
+            return sum;
+        }
+
+        /*
          * Iterates over the line of pixel coordinates between [x0, y0] and [x1, y1]
          * using Bresenham's algorithm.
          * 
@@ -8,7 +88,7 @@ var util = (function() {
          * stopping when process() returns truthily or when the line's endpoint
          * is reached.
          */
-        traceLine: function(x0, y0, x1, y1, process) {
+        , traceLine: function(x0, y0, x1, y1, process) {
             var temp
             ,   steep = Math.abs(y1 - y0) > Math.abs(x1 - x0)
             ,   movingLeft = false;
